208a706bffb3bb20a0599f598adecf2f7577458c
MephistoJB <6490305+MephistoJB@users.noreply.github.com>
2025-02-05 17:14:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -210,4 +210,6 @@ async def receive_data():
             return False
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
