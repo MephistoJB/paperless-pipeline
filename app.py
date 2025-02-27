3eeb7874ae7703038d2e61931131f250e0754b7d
MephistoJB <6490305+MephistoJB@users.noreply.github.com>
2025-02-27 17:55:50 +0100

    Changed Version number

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from services.paperless_api import PaperlessAPI
 app = Flask(__name__)
 
 # Store version in a variable (or load from a config file)
-VERSION = "1.1.1"
+VERSION = "1.2.0"
 
 # Define fixed tags for buttons
 BUTTON_TAGS = {
